ffb9c21dd67a3b6de760e60b7b9a55a482e8303a
ugu5ma <bestell@clarisweb.de>
2016-11-01 11:41:15 +0100

    Initial commit

---
+++ server.js
@@ -0,0 +1,68 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials')
+app.set('view engine', 'hbs');
+
+
+app.use((req, res, next) => {
+var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
+  console.log(log);
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if (err) {
+      console.log('not able to access server.log')
+    }
+  })
+ next();
+});
+
+// app.use((req, res, next) =>{
+//  res.render('maintenance.hbs');
+// });
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () =>{
+ return new Date().getFullYear()
+
+});
+
+hbs.registerHelper('screamIT', (text) =>{
+  return text.toUpperCase();
+
+});
+
+app.get('/', (req, res) =>{
+  res.render('home.hbs', {
+    pageTitle: 'Home Page',
+    welcomeMessage: 'Welcome here',
+
+
+  });
+});
+app.get('/about', (req, res) =>{
+   res.render('about.hbs', {
+     pageTitle: 'About Page',
+
+
+   });
+});
+app.get('/bad', (req, res) =>{
+
+ res.send({
+   Error: 'Error-Page-Bad',
+   Bad: [
+     'This',
+     'Page'
+   ]
+ });
+});
+
+
+app.listen(3000, () => {
+  console.log('Server up and Running on port 3k');
+});
